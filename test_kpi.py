24cf8d80fe2cc5a5b7600042f30af0648d54f58e
sarah fullmer <sarah.fullmer@gmail.com>
2024-05-11 15:40:29 +0200

    update tests

--- test_kpi.py
+++ test_kpi.py
@@ -1,5 +1,13 @@
 import pytest
 import konsumer_pris_indeks
 
-def test_gjennomsnitt_kpi_i_år():
-    assert konsumer_pris_indeks.gjennomsnitt_kpi_i_år(1954) == 7.1
+
+@pytest.fixture
+def kpis():
+   return konsumer_pris_indeks.lager_KPI_objekt_list_fra_data()
+
+def test_gjennomsnitt_kpi_i_år(kpis):
+    assert konsumer_pris_indeks.gjennomsnitt_kpi_i_år(1954, kpis) == 7.1
+
+def test_pris_i_annet_år(kpis):
+    assert round(konsumer_pris_indeks.pris_i_annet_år(2010,2000,45, kpis),2) == 54.89 
